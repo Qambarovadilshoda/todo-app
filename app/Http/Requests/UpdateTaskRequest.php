b511c303a45dce5458ca9c0912e1a1f3554740e0
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-04 21:38:34 +0500

    I added validation to UpdateTaskRequest

--- app/Http/Requests/UpdateTaskRequest.php
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -11,7 +11,7 @@ class UpdateTaskRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class UpdateTaskRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'nullable|string|max:225',
+            'description' => 'nullable|string',
+            'status' => 'nullable|in:pending, cancelled, completed',
         ];
     }
 }
