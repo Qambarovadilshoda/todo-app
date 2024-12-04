2cee930fe833d5caf405783f661333f0e142c55b
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-04 21:33:26 +0500

    I added show method to TaskController

--- app/Http/Controllers/Task/TaskController.php
+++ app/Http/Controllers/Task/TaskController.php
@@ -13,4 +13,12 @@ class TaskController extends Controller
         $tasks = Auth::user()->tasks()->with('user')->paginate(10);
         return $this->responsePagination($tasks, TaskResource::collection($tasks));
     }
+    public function show($id)
+    {
+        $task = $this->findTask($id);
+        if (!$task) {
+            return $this->error('Task not found', 404);
+        }
+        return $this->success(new TaskResource($task->load('user')));
+    }
 }
