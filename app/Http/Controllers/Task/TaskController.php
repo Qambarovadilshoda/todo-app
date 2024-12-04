5195e4de80904469130f7e6e3c4ee18e14f2a9a9
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-04 21:28:54 +0500

    I added index method to TaskController

--- app/Http/Controllers/Task/TaskController.php
+++ app/Http/Controllers/Task/TaskController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\TaskResource;
+use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
-    //
+    public function index()
+    {
+        $tasks = Auth::user()->tasks()->with('user')->paginate(10);
+        return $this->responsePagination($tasks, TaskResource::collection($tasks));
+    }
 }
