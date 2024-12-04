2f4f8e6768012dbd3e58db6c47e25583d83e3299
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-04 21:59:44 +0500

    I added neccesary routes to api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::apiResource('/tasks', TaskController::class);
+    Route::get('/filter/tasks', [TaskController::class, 'tasksFilter']);
+    Route::get('/update/status/{id}', [TaskController::class, 'updateStatus']);
+});
