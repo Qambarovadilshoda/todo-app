f023059406738f1e74bc65e30c6226dad18abb54
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-05 16:36:26 +0500

    I added routes for authentication

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TaskController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Task\TaskController;
 
 Route::middleware('auth:sanctum')->group(function(){
     Route::get('/user', function (Request $request) {
@@ -12,3 +14,4 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::get('/filter/tasks', [TaskController::class, 'tasksFilter']);
     Route::get('/update/status/{id}', [TaskController::class, 'updateStatus']);
 });
+Route::post('/login', [AuthController::class, 'login']);
