b68f1a1ab18296c977c681e4d19558a704bbcff7
azizovmuhammadali <alico.777777@gmail.com>
2024-12-07 13:54:32 +0500

    Additional part of the Api route

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,7 @@ use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Task\TaskController;
 
 Route::middleware('auth:sanctum')->group(function(){
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+    Route::get('/user',[AuthController::class,'findUser']);
     Route::apiResource('/tasks', TaskController::class);
     Route::get('/filter/tasks', [TaskController::class, 'tasksFilter']);
     Route::get('/update/status/{id}', [TaskController::class, 'updateStatus']);
