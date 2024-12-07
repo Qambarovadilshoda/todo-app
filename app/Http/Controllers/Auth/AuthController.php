ab9dd73dd80aab4cbb4aedf1c450049e908eafb0
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-07 12:38:26 +0500

    I formatted the code

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -11,11 +11,11 @@ use App\Mail\EmailVerify;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Mail;
 
 class AuthController extends Controller
 {
-    public function register(AuthRegisterRequest $request){
+    public function register(AuthRegisterRequest $request)
+    {
         $requestData = $request->validated();
         $user = new User();
         $user->name = $requestData['name'];
@@ -26,28 +26,31 @@ class AuthController extends Controller
         SendEmail::dispatch($user);
         return $this->success(new UserResource($user));
     }
-    public function emailVerify(Request $request){
+    public function emailVerify(Request $request)
+    {
         $user = User::where('verification_token', $request->token)->first();
-        if(!$user || $user->verification_token !== $request->token){
-           return $this->error();
+        if (!$user || $user->verification_token !== $request->token) {
+            return $this->error();
         }
         $user->email_verified_at = now();
         $user->save();
         return $this->success($user);
     }
-    public function login(LoginRequest $request){
-    $user = User::where('email',$request->email)->first();
-    if (!$user->hasVerifiedEmail()) {
-      return $this->error('You must verify your email');
-  }
-    if(Hash::check($request->password,$user->password)){
-      $user->tokens()->delete();
-      $token = $user->createToken('auth_login')->plainTextToken;
-      return $this->success([new UserResource($user), $token]);
+    public function login(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if (!$user->hasVerifiedEmail()) {
+            return $this->error('You must verify your email');
+        }
+        if (Hash::check($request->password, $user->password)) {
+            $user->tokens()->delete();
+            $token = $user->createToken('auth_login')->plainTextToken;
+            return $this->success([new UserResource($user), $token]);
+        }
     }
-}
-public function logout(Request $request){
-    $request->user()->currentAccessToken()->delete();
-    return $this->success('Successfully logouted');
-     }
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return $this->success('Successfully logouted');
+    }
 }
